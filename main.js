34da0340bb0f0a0b7c7f2ace67875d08e360fb91
Kris Wong <krischwong@gmail.com>
2022-09-11 01:47:37 -0400

    Initial commit, setup basic webpack

---
+++ main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const words = document.createElement("div");
+words.innerHTML = "123";
+
+document.body.appendChild(words);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50djIvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgd29yZHMgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiZGl2XCIpO1xud29yZHMuaW5uZXJIVE1MID0gXCIxMjNcIjtcblxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZCh3b3Jkcyk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
