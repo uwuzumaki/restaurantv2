69dd9fa21eb83d30f33e9fec422fe06379c3521c
Kris Wong <krischwong@gmail.com>
2022-09-26 00:24:25 -0400

    Out of ideas to imrpove, done for now

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -11,6 +11,18 @@ const Contact = () => {
   title.innerHTML = "Contact";
   homeCenter.appendChild(title);
 
+  const contact1 = document.createElement("div");
+  contact1.classList = "contact";
+  homeCenter.appendChild(contact1);
+
+  const name = document.createElement("h5");
+  name.innerHTML = "Irene";
+  contact1.append(name);
+
+  const number = document.createElement("p");
+  number.innerHTML = "(905) 123 - 1234";
+  contact1.appendChild(number);
+
   return wrapper;
 };
 
